1b484c8eb4493ef52b06d639349ad588f712700c
Tero Saarni <tero.saarni@est.tech>
2022-10-07 09:28:06 +0300

    jruby-openssl notes

--- logstash/README-jruby-openssl.txt
+++ logstash/README-jruby-openssl.txt
@@ -29,7 +29,7 @@ OpenSSL::PKey::read(File.read("/home/tsaarni/work/devenvs/logstash/certs/server-
 
 
 
-# unittest 
+# unittest
 # from: .github/workflows/ci-test.yml
 
 jruby -S bundle install
@@ -37,6 +37,10 @@ jruby -rbundler/setup -S rake test_prepare
 jruby -rbundler/setup -S rake test
 
 jruby -Ilib:src/test/ruby src/test/ruby/ec/test_ec.rb
+
+# to recompile (running test again will pick the newly compiled version automatically)
+mvn clean package -Dmaven.test.skip=true
+
 
 
 
@@ -54,4 +58,10 @@ digest = OpenSSL::Digest::SHA256.new
 sig = key.sign(digest, data)
 key.verify(digest, sig, data)
 
+
+
+
+# convert PKCS#8 to encrypted
+openssl pkcs8 -in private_key_pkcs8.pem -topk8 -out private_key_pkcs8_enc.pem 
+openssl pkcs8 -in private_key_pkcs8.pem -topk8 -out private_key_pkcs8_enc.pem -v1 PBE-MD5-DES  # PBES1
 
--- logstash/tls-client.rb
+++ logstash/tls-client.rb
@@ -1,5 +1,6 @@
 # Run server that requires client to send client cert:
-#   openssl s_server -accept 8443 -cert certs/server.pem -key certs/server-key.pem -tls1_2 -Verify 1
+#   openssl s_server -accept 8443 -cert certs/server.pem -key certs/server-key.pem -CAfile certs/client-ca.pem -tls1_2 -Verify 1
+#   openssl s_server -accept 8443 -cert certs/server-revoked.pem -key certs/server-revoked-key.pem -CAfile certs/client-ca.pem -tls1_2 -Verify 1
 #
 # Then run this script.
 #
@@ -15,11 +16,29 @@ ssl_context.key = OpenSSL::PKey::read(File.read("/home/tsaarni/work/devenvs/logs
 
 cert_store = OpenSSL::X509::Store.new
 cert_store.add_file("/home/tsaarni/work/devenvs/logstash/certs/server-ca.pem")
+
+# copy the behavior of X509_load_crl_file() which supports loading bundles of CRLs
+END_TAG = "\n-----END X509 CRL-----\n"
+File.read("/home/tsaarni/work/devenvs/logstash/certs/crl.pem").split(END_TAG).each do |crl|
+    crl << END_TAG
+    cert_store.add_crl(OpenSSL::X509::CRL.new(crl))
+end
+
+#cert_store.add_crl(OpenSSL::X509::CRL.new(File.read("/home/tsaarni/work/devenvs/logstash/certs/server-ca-crl.pem")))
+#cert_store.add_crl(OpenSSL::X509::CRL.new(File.read("/home/tsaarni/work/devenvs/logstash/certs/client-ca-crl.pem")))
+#cert_store.add_crl(OpenSSL::X509::CRL.new(File.read("/home/tsaarni/work/devenvs/logstash/certs/crl.pem"))) # bundle
+cert_store.flags = OpenSSL::X509::V_FLAG_CRL_CHECK | OpenSSL::X509::V_FLAG_CRL_CHECK_ALL
+
 ssl_context.cert_store = cert_store
 ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER|OpenSSL::SSL::VERIFY_FAIL_IF_NO_PEER_CERT
+#ssl_context.verify_callback = -> (preverify_ok, ssl_context) {
+#    cert_store.verify(ssl_context.current_cert)
+#}
 
 socket = TCPSocket.new("localhost", "8443")
 
 socket = OpenSSL::SSL::SSLSocket.new(socket, ssl_context)
 
 socket.connect
+socket.puts "Hello World!"
+socket.close
