1340794d2bdc2a319524c10a1dce16bc790ddc1f
Tero Saarni <tero.saarni@est.tech>
2024-11-29 12:54:52 +0200

    bugfix for gunicorn context

--- gunicorn/configs/gunicorn-hotreload-conf.py
+++ gunicorn/configs/gunicorn-hotreload-conf.py
@@ -1,7 +1,10 @@
 def ssl_context(conf, default_ssl_context_factory):
     import ssl
     context = default_ssl_context_factory()
-    def sni_callback(conn, domain, context):
-        context.load_cert_chain(conf.certfile, conf.keyfile)
+    def sni_callback(socket, server_hostname, context):
+        new_context = default_ssl_context_factory()
+        new_context.load_cert_chain(conf.certfile, conf.keyfile)
+        socket.context = new_context
+
     context.sni_callback = sni_callback
     return context
